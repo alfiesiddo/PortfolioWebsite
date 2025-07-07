6a243bd16613a2727ede6b7d0af61ca3e8d612e9
alfiesiddo <alfiesiddons@gmail.com>
2025-07-07 16:18:39 +0100

    added Projects, About, Contact pages with usable nav links.

--- PortfolioWebsite/Controllers/HomeController.cs
+++ PortfolioWebsite/Controllers/HomeController.cs
@@ -17,11 +17,19 @@ namespace PortfolioWebsite.Controllers
         {
             return View();
         }
-
-        public IActionResult Privacy()
+        public IActionResult About()
         {
             return View();
         }
+        public IActionResult Projects()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
